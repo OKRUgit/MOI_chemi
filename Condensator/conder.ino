7c8d685d06a006710880d72d2f4e61cec77f7bc1
OKRUgit <git@ok-ru.ru>
2025-11-16 20:32:52 +0300

    txt-3

--- Condensator/conder.ino
+++ Condensator/conder.ino
@@ -19,34 +19,39 @@ void loop(){
     digitalWrite(13,HIGH);
   }
 
-  if(mk==1){
-    pinMode(3,OUTPUT);
-    pinMode(13,INPUT);
-    digitalWrite(3,HIGH);
-  }
+  if(mk==1)
+    {
+     pinMode(3,OUTPUT);
+     pinMode(13,INPUT);
+     digitalWrite(3,HIGH);
+    }
 
       time0=micros();
-      while(analogRead(A0)<644){
-      time2=micros()-time0;
-      if(time2>=1000000 && mk==0){
-      mk=1;
-      time0=100000000;
-      break;
-    }
-  }
+    while(analogRead(A0)<644)
+      {
+       time2=micros()-time0;
+       if(time2>=1000000 && mk==0)
+       {
+         mk=1;
+         time0=100000000;
+         break;
+       }
+      }
 
   time1=micros()-time0; 
 
-  while(analogRead(A0)>0){ 
+  while(analogRead(A0)>0)
+  { 
     pinMode(3,OUTPUT); 
     pinMode(13,OUTPUT); 
     digitalWrite(3,LOW); 
     digitalWrite(13,LOW);
   }
 
-  if(mk==1&&time1<1000){
-    mk=0;
-  }
+  if(mk==1&&time1<1000)
+    {
+      mk=0;
+    }
 
   lcd.setCursor(1,0);
   c=time1;
@@ -57,16 +62,18 @@ void loop(){
     lcd.setCursor(1,0);
     lcd.print(" TEST uF   "); 
   }
-  else{
-    lcd.print(c); 
+  else{                            //Вывод на LDC 
+       lcd.print(c); 
 
-    if(mk==0){
-      lcd.print(" nF       ");
-    }
-    if(mk==1){
-      lcd.print(" uF       ");
-    }
-  }
+       if(mk==0)
+       {
+         lcd.print(" nF       ");
+       }
+        if(mk==1)
+       {
+         lcd.print(" uF       ");
+       }
+     } 
   if(i==0){                         // калибровка нуля
     i++;
     null0=c+0.02;
